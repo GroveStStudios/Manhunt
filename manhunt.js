0cafffb01fa91ec13a3971f054a1cd712c99bf61
Sean Thomas Sullivan <sullivansean27@gmail.com>
2017-08-03 06:10:49 -0700

    some game core reorganizing
    and player class and stuff

--- manhunt.js
+++ manhunt.js
@@ -69,42 +69,6 @@ function makeTexture(gl){
   return texture;
 }
 
-//draw octogonal tile from bottom-left anchor point
-/* ----------------
- * -----10___11----
- * ----/------\----
- * ---6--7--8--9---
- * ---|--------|---
- * ---2--3--4--5---
- * ----\------/----
- * -----0___1------
- * ----------------
- */
-function drawTile(gl,x,y,s){
-  function mult(x){
-    return x*s;
-  }
-  //map from vertex no. to distance from anchor in x/y dir
-  var w = [1,2.71,0,1,2.71,3.71,0,1,2.71,3.71,1,2.71].map(mult);
-  var h = [0,0,1,1,1,1,2.71,2.71,2.71,2.71,3.71,3.71].map(mult);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    x+w[0], y+h[0], x+w[2], y+h[2], x+w[3], y+h[3],
-    x+w[0], y+h[0], x+w[3], y+h[3], x+w[4], y+h[4],
-    x+w[0], y+h[0], x+w[4], y+h[4], x+w[1], y+h[1],
-    x+w[1], y+h[1], x+w[4], y+h[4], x+w[5], y+h[5],
-    x+w[2], y+h[2], x+w[6], y+h[6], x+w[7], y+h[7],
-    x+w[2], y+h[2], x+w[7], y+h[7], x+w[3], y+h[3],
-    x+w[3], y+h[3], x+w[7], y+h[7], x+w[8], y+h[8],
-    x+w[3], y+h[3], x+w[8], y+h[8], x+w[4], y+h[4],
-    x+w[4], y+h[4], x+w[8], y+h[8], x+w[9], y+h[9],
-    x+w[4], y+h[4], x+w[9], y+h[9], x+w[5], y+h[5],
-    x+w[6], y+h[6], x+w[10], y+h[10], x+w[7], y+h[7],
-    x+w[7], y+h[7], x+w[10], y+h[10], x+w[11], y+h[11],
-    x+w[7], y+h[7], x+w[11], y+h[11], x+w[8], y+h[8],
-    x+w[8], y+h[8], x+w[11], y+h[11], x+w[9], y+h[9]]),
-    gl.STATIC_DRAW);
-}
-
 $(document).keydown(function(event){
   if(event.which==87 || event.which==38) { //UP
     playerAccel.y += 1;
@@ -159,7 +123,7 @@ function main(){
 
   //shader attributes
   var uvBuf = setShaderAttribute(gl, prog, "uvcoord");
-  drawTile(gl,0,0,1/3.71); //draw tile UVs, this is the same for all tiles
+  draw(gl,0,0,1/3.71); //draw tile UVs, this is the same for all tiles
   var posBuf = setShaderAttribute(gl, prog, "pos");
 
   //create / bind texture
@@ -198,6 +162,97 @@ function updateInfo(){
   $("#speed").text("Speed: "+playerSpeed);
 }
 
+//game instance constructor
+var gameCore = function(gameInstance){
+  this.instance = gameInstance;
+  this.server = this.instance !== undefined;
+
+  this.world = {
+    width : 720,
+    height: 480
+  }
+
+  if(this.server){
+    this.players = {
+      self: new gamePlayer(this,this.instance.host),
+      others: this.instance.clients.map(function(client){
+        return new gamePlayer(this,client);
+      });
+    };
+    this.players.self.pos = {x:20,y:20};
+  } else{
+    this.players = {
+      self: new gamePlayer(this),
+      others: []
+    };
+    this.ghosts = {
+      serverPosSelf : new gamePlayer(this),
+      serverPosOthers : [],
+      posOthers: []
+    }
+
+    this.ghosts.posOther.state = 'dest_pos';
+
+    this.ghosts.serverPosSelf.state = 'server_pos';
+    this.ghosts.serverPosOther.state = 'server_pos';
+
+    this.ghosts.serverPosSelf.pos = { x:20, y:20 };
+
+    this.serverUpdates = [];
+
+  }
+
+  this.playerspeed = 120;
+
+  this._pdt = 0.0001;
+  this._pdte = new Date().getTime();
+  this.local_time = 0.016;
+  this._dt = new Date().getTime();
+  this._dte = new Date().getTime();
+
+  this.createPhysicsSimulation();
+  this.createTimer();
+
+}
+
+if('undefined' != typeof global){
+  module.exports = global.gameCore = gameCore;
+}
+
+Number.prototype.fixed = function(n) { n = n || 3; return parseFloat(this.toFixed(n)); };
+    //copies a 2d vector like object from one to another
+gameCore.prototype.pos = function(a) { return {x:a.x,y:a.y}; };
+    //Add a 2d vector with another one and return the resulting vector
+gameCore.prototype.v_add = function(a,b) { return { x:(a.x+b.x).fixed(), y:(a.y+b.y).fixed() }; };
+    //Subtract a 2d vector with another one and return the resulting vector
+gameCore.prototype.v_sub = function(a,b) { return { x:(a.x-b.x).fixed(),y:(a.y-b.y).fixed() }; };
+    //Multiply a 2d vector with a scalar value and return the resulting vector
+gameCore.prototype.v_mul_scalar = function(a,b) { return {x: (a.x*b).fixed() , y:(a.y*b).fixed() }; };
+    //For the server, we need to cancel the setTimeout that the polyfill creates
+gameCore.prototype.stop_update = function() {  window.cancelAnimationFrame( this.updateid );  };
+    //Simple linear interpolation
+gameCore.prototype.lerp = function(p, n, t) { var _t = Number(t); _t = (Math.max(0, Math.min(1, _t))).fixed(); return (p + _t * (n - p)).fixed(); };
+    //Simple linear interpolation between 2 vectors
+gameCore.prototype.v_lerp = function(v,tv,t) { return { x: this.lerp(v.x, tv.x, t), y:this.lerp(v.y, tv.y, t) }; };
+
+gameCore.prototype.update = function(t) {
+        //Work out the delta time
+    this.dt = this.lastframetime ? ( (t - this.lastframetime)/1000.0).fixed() : 0.016;
+
+        //Store the last frame time
+    this.lastframetime = t;
+
+        //Update the game specifics
+    if(!this.server) {
+        this.client_update();
+    } else {
+        this.server_update();
+    }
+        //schedule the next update
+    this.updateid = window.requestAnimationFrame( this.update.bind(this), this.viewport );
+}; //game_core.update
+
+
 //player class constructor
 var gamePlayer = function(gameInstance, playerInstance){
   this.instance = playerInstance;
@@ -209,13 +264,60 @@ var gamePlayer = function(gameInstance, playerInstance){
   this.speed = 0; //square of player speed
 
   this.color = 'rgba(255,255,255,1)';
+  this.size = 3;
   //other things the player has
-})
+
+  //draw octogonal tile from bottom-left anchor point
+  /* ----------------
+   * -----10___11----
+   * ----/------\----
+   * ---6--7--8--9---
+   * ---|--------|---
+   * ---2--3--4--5---
+   * ----\------/----
+   * -----0___1------
+   * ----------------
+   */
+  this.draw = function(gl){
+    function mult(x){
+      return x*this.size;
+    }
+    //map from vertex no. to distance from anchor in x/y dir
+    var w = [1,2.71,0,1,2.71,3.71,0,1,2.71,3.71,1,2.71].map(mult);
+    var h = [0,0,1,1,1,1,2.71,2.71,2.71,2.71,3.71,3.71].map(mult);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+      this.pos.x+w[0], this.pos.y+h[0], this.pos.x+w[2],
+      this.pos.y+h[2], this.pos.x+w[3], this.pos.y+h[3],
+      this.pos.x+w[0], this.pos.y+h[0], this.pos.x+w[3],
+      this.pos.y+h[3], this.pos.x+w[4], this.pos.y+h[4],
+      this.pos.x+w[0], this.pos.y+h[0], this.pos.x+w[4],
+      this.pos.y+h[4], this.pos.x+w[1], this.pos.y+h[1],
+      this.pos.x+w[1], this.pos.y+h[1], this.pos.x+w[4],
+      this.pos.y+h[4], this.pos.x+w[5], this.pos.y+h[5],
+      this.pos.x+w[2], this.pos.y+h[2], this.pos.x+w[6],
+      this.pos.y+h[6], this.pos.x+w[7], this.pos.y+h[7],
+      this.pos.x+w[2], this.pos.y+h[2], this.pos.x+w[7],
+      this.pos.y+h[7], this.pos.x+w[3], this.pos.y+h[3],
+      this.pos.x+w[3], this.pos.y+h[3], this.pos.x+w[7],
+      this.pos.y+h[7], this.pos.x+w[8], this.pos.y+h[8],
+      this.pos.x+w[3], this.pos.y+h[3], this.pos.x+w[8],
+      this.pos.y+h[8], this.pos.x+w[4], this.pos.y+h[4],
+      this.pos.x+w[4], this.pos.y+h[4], this.pos.x+w[8],
+      this.pos.y+h[8], this.pos.x+w[9], this.pos.y+h[9],
+      this.pos.x+w[4], this.pos.y+h[4], this.pos.x+w[9],
+      this.pos.y+h[9], this.pos.x+w[5], this.pos.y+h[5],
+      this.pos.x+w[6], this.pos.y+h[6], this.pos.x+w[10],
+      this.pos.y+h[10], this.pos.x+w[7], this.pos.y+h[7],
+      this.pos.x+w[7], this.pos.y+h[7], this.pos.x+w[10],
+      this.pos.y+h[10], this.pos.x+w[11], this.pos.y+h[11],
+      this.pos.x+w[7], this.pos.y+h[7], this.pos.x+w[11],
+      this.pos.y+h[11], this.pos.x+w[8], this.pos.y+h[8],
+      this.pos.x+w[8], this.pos.y+h[8], this.pos.x+w[11],
+      this.pos.y+h[11], this.pos.x+w[9], this.pos.y+h[9]]),
+      gl.STATIC_DRAW);
+  };
+};
 
 const accelScaling = 0.07;
 const maxSpeed = 4.0;
-var playerPos = {x:0, y:0};
-var playerAccel = {x:0, y:0};
-var playerVel = {x:0, y:0};
-var playerSpeed = 0; //square of player speed
 main();
